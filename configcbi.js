6c3a8b030a4d801cf1b4cb172e38735338facdf5
Irbeem <96011414+Irbeem@users.noreply.github.com>
2025-09-03 17:40:46 +0700

    Update configcbi.js

--- configcbi.js
+++ configcbi.js
@@ -1,1 +1,13 @@
+// ====== config.js ======
+window.CREDENTIALS = [
+  { user: "bee",   pass: "1234" },
+  { user: "cscbi", pass: "cs2025" },
+];
 
+window.AUTH_KEY = "cs_calendar_auth_user";
+
+// ชีต
+window.SHEET_ID = "1Yv7-SwI9Iuv9QcEaqFVCEejEe3_8Ja77NNwrM61x-oA";
+window.CALENDAR_SHEET = "Carlendar";
+window.NAMES_SHEET = "Names";
+window.SpecialHolidays_SHEET = "SpecialHolidays";
