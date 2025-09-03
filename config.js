c64c728d3715860ef58eb64c788fd57566e2d1b1
Irbeem <96011414+Irbeem@users.noreply.github.com>
2025-09-03 17:06:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+// ====== config.js ======
+// ปรับ user/pass ที่นี่
+window.CREDENTIALS = [
+  { user: "bee",   pass: "1234" },
+  { user: "csbkk", pass: "cs2025" },
+];
 
+// คีย์เก็บสถานะล็อกอิน
+window.AUTH_KEY = "cs_calendar_auth_user";
+
+// ตั้งค่าการเชื่อมต่อชีต
+window.SHEET_ID = "1ySiuPMVyrNOpNxx5fR0olf2Dbaq84zKVlVaVo6mFBi8";
+window.CALENDAR_SHEET = "Carlendar";
+window.NAMES_SHEET = "Names";
+window.SpecialHolidays_SHEET = "SpecialHolidays";
